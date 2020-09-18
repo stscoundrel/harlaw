42c7e2312543a7f46471aaa3d0a23e70e2e920f1
stscoundrel <sampo@pixels.fi>
2020-09-18 21:38:36 +0300

    Update default removes & replaces

--- src/constants/settings.js
+++ src/constants/settings.js
@@ -1,6 +1,6 @@
 const { TAB, MEANINGS } = require('./characters.js')
 
-const REMOVES = [...MEANINGS, TAB, '[c gray]', '[/c]', '\\']
+const REMOVES = [...MEANINGS, TAB, '[c gray]', '[/c]', '\\', '[trn]', '[/trn]']
 const REPLACES = [
   { search: '[b]', replace: '<strong>' },
   { search: '[/b]', replace: '</strong>' },
@@ -21,7 +21,7 @@ const DEFAULT_SETTINGS = {
 const NO_MARKUP_SETTINGS = {
   replaces: [],
   removes: [
-    '\\', '[b]', '[/b]', '[i]', '[/i]', '[p]', '[/p]', '[ref]', '[/ref]', '[/m]', '[m1]', '[m2]', '[m3]', '[m4]', '[m5]', '[m6]', '[m7]', '[m8]', '[m9]', '[m10]', '\t', '[c gray]', '[/c]',
+    ...MEANINGS, '\\', '[b]', '[/b]', '[i]', '[/i]', '[p]', '[/p]', '[ref]', '[/ref]', '[/m]',
   ],
 }
 
